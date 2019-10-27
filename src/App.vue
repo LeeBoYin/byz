d0b3d3f9d2cd5983309ba27b305da1a6cbfa5f56
leeboyin <leeboyin945@gmail.com>
2019-10-27 23:16:21 +0800

    setup new project

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+	<div id="app">
+		<h1>
+			{{ message }}
+		</h1>
+		<input type="text" v-model="message">
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				message: 'Hello World',
+			};
+		},
+	};
+</script>
+
+<style>
+	#app {
+		font-size: 18px;
+		font-family: 'Roboto', sans-serif;
+		color: blue;
+	}
+</style>
