58ecb136721cba1fdf519a4a595534558d92ec68
leeboyin <leeboyin945@gmail.com>
2020-05-31 17:56:27 +0800

    delete comments on post deleted by cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -14,8 +14,21 @@ export const onCreateComment = functions.firestore
 			console.error('postRef not exist');
 			return null;
 		}
-		return postRef.update({
-			commentCount: admin.firestore.FieldValue.increment(1),
+		return new Promise(async (resolve, reject) => {
+			try {
+				// check post exist first
+				const postSnapshot = await postRef.get();
+				if(!postSnapshot.exists) {
+					resolve();
+				}
+				// increment commentCount
+				await postRef.update({
+					commentCount: admin.firestore.FieldValue.increment(1),
+				});
+				resolve();
+			} catch (e) {
+				reject();
+			}
 		});
 	});
 
@@ -27,7 +40,43 @@ export const onDeleteComment = functions.firestore
 			console.error('postRef not exist');
 			return null;
 		}
-		return postRef.update({
-			commentCount: admin.firestore.FieldValue.increment(-1),
+		return new Promise(async (resolve, reject) => {
+			try {
+				// check post exist first
+				const postSnapshot = await postRef.get();
+				if(!postSnapshot.exists) {
+					resolve();
+				}
+				// decrement commentCount
+				await postRef.update({
+					commentCount: admin.firestore.FieldValue.increment(-1),
+				});
+				resolve();
+			} catch (e) {
+				reject();
+			}
+		});
+	});
+
+export const onDeletePost = functions.firestore
+	.document('/board/{boardId}/posts/{postId}')
+	.onDelete(async (snapshot, context) => {
+		const commentsRef = snapshot.ref.collection('comments');
+		return new Promise(async (resolve, reject) => {
+			try {
+				const commentsSnapshot = await commentsRef.get();
+				if(commentsSnapshot.empty) {
+					resolve();
+				}
+				// delete all comments
+				const deletePromises: Promise<any>[] = [];
+				commentsSnapshot.forEach(function(doc) {
+					deletePromises.push(doc.ref.delete());
+				});
+				await Promise.all(deletePromises);
+				resolve();
+			} catch (e) {
+				reject();
+			}
 		});
 	});
